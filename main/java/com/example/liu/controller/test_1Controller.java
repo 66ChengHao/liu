266ce0530706057706cc2c1550ce618f944ceec0
me <3507634684@qq.com>
2021-07-09 13:49:54 +0800

    Initial commit

---
+++ main/java/com/example/liu/controller/test_1Controller.java
@@ -0,0 +1,12 @@
+package com.example.liu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test_1Controller {
+    @Autowired
+    @GetMapping("/hello")
+    public String test_1(){return "hello test_1";}
+}
